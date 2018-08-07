b26f454ec63adc061c968bcd1ca468b857af1f17
Michal Klisch <michal.klisch@gmail.com>
2018-08-07 21:41:52 +0200

    Initial commit

---
+++ Classes/Time.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BerlinClock
+{
+    internal class Time
+    {
+        public int Hour { get; internal set; }
+        public int Minute { get; internal set; }
+        public int Second { get; internal set; }
+
+        public static bool TryParse(string time, out Time parsedTime)
+        {
+            parsedTime = null;
+            //split by :
+            var timeParts = time.Split(':');
+            if (timeParts.Length != 3)
+                return false;
+            //check hour is within 0-24 and an integer
+            int hour;
+            if (!int.TryParse(timeParts[0], out hour) || hour < 0 || hour > 24)
+                return false;
+            //check minutes are within 0-59 and an integer
+            int minute;
+            if (!int.TryParse(timeParts[1], out minute) || minute < 0 || minute > 59)
+                return false;
+            //check seconds are within 0-59 and an integer
+            int second;
+            if (!int.TryParse(timeParts[2], out second) || second < 0 || second > 59)
+                return false;
+            parsedTime = new Time() {Hour = hour, Minute = minute, Second = second};
+            return true;
+        }
+    }
+}
---
+++ Classes/TimeConverter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+
+namespace BerlinClock
+{
+    public class TimeConverter : ITimeConverter
+    {
+        public string convertTime(string aTime)
+        {
+            //try parsing input time
+            Time time;
+            if (!Time.TryParse(aTime, out time))
+                throw new Exception("Unknown time format");
+            return GetLine1(time) + GetLine2(time) + GetLine3(time) + GetLine4(time) +
+                   GetLine5(time);
+        }
+
+        private static string GetLine1(Time time)
+        {
+            return (time.Second % 2 == 0 ? "Y" : "O") + Environment.NewLine;
+        }
+
+        private static string GetLine2(Time time)
+        {
+            return new string('R', time.Hour / 5).PadRight(4,'O') + Environment.NewLine;
+        }
+
+        private static string GetLine3(Time time)
+        {
+            return new string('R', time.Hour % 5).PadRight(4, 'O') + Environment.NewLine;
+        }
+
+        private static string GetLine4(Time time)
+        {
+            var sb = new StringBuilder();
+            for (var i = 0; i < 11; i++)
+            {
+                if (time.Minute / 5 > i)
+                    sb.Append(i % 3 == 2 ? 'R' : 'Y');
+                else
+                {
+                    sb.Append('O');
+                }
+            }
+            sb.Append(Environment.NewLine);
+            return sb.ToString();
+        }
+
+        private static string GetLine5(Time time)
+        {
+            return new string('Y', time.Minute % 5).PadRight(4, 'O');
+        }
+
+
+    }
+}
